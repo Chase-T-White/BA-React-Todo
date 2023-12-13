00963451c1e9f59ade357d9882fb01c681bd3ba8
cwhite70 <chase.t.white@hotmail.com>
2023-12-13 00:12:16 -0700

    first commit

---
+++ src/context/taskContext.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect, useContext } from "react";
+import { uid } from "uid";
+import { checkStorage, updateStorage } from "../functions/storageFunctions";
+
+const TasksContext = React.createContext();
+
+const TasksProvider = ({ children }) => {
+  const [tasksList, setTasksList] = useState([]);
+  const [isFirstLoad, setIsFirstLoad] = useState(true);
+
+  useEffect(() => {
+    checkStorage(setTasksList);
+  }, []);
+
+  useEffect(() => {
+    if (isFirstLoad) {
+      setIsFirstLoad(false);
+      return;
+    }
+    updateStorage(tasksList);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [tasksList]);
+
+  const createTask = (newTaskData) => {
+    const newTask = { ...newTaskData, id: uid(), isCompleted: false };
+    setTasksList([...tasksList, newTask]);
+  };
+
+  const updatedTask = (id) => {};
+
+  const deleteSingleTask = (id) => {
+    const newTaskList = tasksList.filter((task) => task.id !== id);
+    setTasksList([newTaskList]);
+  };
+
+  const deleteTasks = () => {
+    setTasksList([]);
+  };
+
+  return (
+    <TasksContext.Provider
+      value={{
+        tasksList,
+        createTask,
+        updatedTask,
+        deleteSingleTask,
+        deleteTasks,
+      }}
+    >
+      {children}
+    </TasksContext.Provider>
+  );
+};
+
+export const useTasksContext = () => {
+  return useContext(TasksContext);
+};
+
+export { TasksContext, TasksProvider };
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+import Home from "./Home";
+import TaskPage from "./City";
+import Error from "./ErrorPage";
+
+export { Home, TaskPage, Error };
